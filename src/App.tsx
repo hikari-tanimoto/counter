62638a4662dbb58933d628508d1faaf46ed8eab9
hikari-tanimoto <hikari-tanimoto@gaji.jp>
2026-04-02 10:01:04 +0900

    Add Zustand for state management, refactor counter components to use store, and update package dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,16 @@
 import { AdjustableCounter } from "@/components/AdjustableCounter";
 import { BasicCounter } from "@/components/BasicCounter";
-import { CounterProvider } from "@/contexts/CounterContext";
-import { adjustableReducer } from "@/reducers/adjustableReducer";
-import { basicReducer } from "@/reducers/basicReducer";
-import { useReducer } from "react";
-
-type State = {
-  basic: { count: number };
-  adjustable: { count: number; step: number };
-};
-
-const initialState: State = {
-  basic: { count: 0 },
-  adjustable: { count: 0, step: 1 },
-};
+import { useCounterStore } from "@/stores/counterStore";
 
 function App() {
-  const [basicState, basicDispatch] = useReducer(
-    basicReducer,
-    initialState.basic,
-  );
-  const [adjustableState, adjustableDispatch] = useReducer(
-    adjustableReducer,
-    initialState.adjustable,
-  );
+  const { basicCount, adjustableCount } = useCounterStore();
 
   return (
     <div>
       <h1>カウンター</h1>
-      <h2>カウンターの合計値：{basicState.count + adjustableState.count}</h2>
-      <CounterProvider
-        value={{
-          basic: basicState,
-          adjustable: adjustableState,
-          basicDispatch,
-          adjustableDispatch: adjustableDispatch,
-        }}
-      >
-        <BasicCounter />
-        <AdjustableCounter />
-      </CounterProvider>
+      <h2>カウンターの合計値：{basicCount + adjustableCount}</h2>
+      <BasicCounter />
+      <AdjustableCounter />
     </div>
   );
 }
